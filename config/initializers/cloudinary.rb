3d84b258b4f39e5754cd869337e623cb22dd755f
Renato Rosa Franco <renato.franco@repassa.com.br>
2022-02-13 20:01:51 -0800

    dotenv and env variables added to the project

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Cloudinary.config do |config|
-	config.cloud_name = 'portal-fronteiri-o'
-	config.api_key = '878763589699628'
-	config.api_secret = 'uO2VaoFHUvFQDpTLiOiiDC_b-5E'
-	config.secure = true
+	config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+	config.api_key 		= ENV['CLOUDINARY_API_KEY']
+	config.api_secret = ENV['CLOUDINARY_API_SECRET']
+	config.secure 		= ENV['CLOUDINARY_SECURE']
 end
